6e362d60e3f02c311f0449b65d3a7774931afd72
Xiangyi Li <venusli370306@gmail.com>
2023-09-30 01:12:07 -0400

    Update resume link

--- data/general.ts
+++ data/general.ts
@@ -16,7 +16,7 @@ export const generalData = {
     {
       label: "Resume",
       value: "Xiangyi_li_resume.pdf",
-      href: "https://github.com/l1xiangyi/xiangyi.li/blob/resume/Xiangyi_li_resume.pdf",
+      href: "https://www.xiangyi.li/Xiangyi_LI_0926_SDE_print.pdf",
     },
     {
       label: "Github",
