849c90e49351ab1abd9cee33f1b0112de92f6669
Xiangyi Li <venusli370306@gmail.com>
2023-10-16 23:50:45 -0700

    Updated some ML experiences and resume (10/14 SDE)

--- data/content.ts
+++ data/content.ts
@@ -16,6 +16,12 @@ export const contentData = [
         date: "05/22 - 11/22",
         description: "Extended Kubernetes APIs for automation in Golang for two OpenShift releases. Built OpenShift Dynamic Plugins using React and TypeScript for better feature visibility. Developed Cypress scripts for the OpenShift web console. Actively participated in debugging Jenkins CI, fixing Ruby testing scripts, and setting up AWS network environments.  ",
       },
+      {
+        title: "Research Assistant at CUHK",
+        subTitle: "Shenzhen, China",
+        date: "02/22 - 05/22",
+        description: "Implemented statistical algorithms like gradient descent and canonical correlation analysis in C++ and Python. Compiled Python code for auto-vectorization for improved computation speed. Distributed VMs for matrix calculations. "
+      },
     ],
   },
   {
@@ -36,14 +42,39 @@ export const contentData = [
     ],
   },
     // @NOTE: You can add more sections here
+    // VOC2012 Object Detection Task – Deep Learning Course Project
     {
       title: "Projects",
       items: [
+        {
+          title: "Better Diffusion",
+          subTitle: "Work in progress",
+          date: "Oct 2023",
+          description: "Scraped, cleaned, and analyzed with NLP techniques on Midjourney's Discord messages. Used them for fine-tuning GPT-3.5. Generate prompts with OpenAI Function API and then images using stable diffusion. "
+        },
+        {
+          title: "Ask Abe AI",
+          subTitle: "https://ask-abe-beta.vercel.app/",
+          date: "Sep 2023 - Present",
+          description: "Developed a better law answering agents. Scraped law data with TypeScript and generated vector embedding with OpenAI embedding API. Used Supabase and pgvector with HNSW for RAG. Built the frontend using React Server Components and TypeScript. ",
+        },
         {
           title: "PadCoder",
           subTitle: "https://github.com/l1xiangyi/padcoder/",
           date: "May 2023",
           description: "Built an iPad LeetCode grinding app using React Native and TypeScript with another engineer. Participated in designing using Figma. Sold to an early stage startup by my partner.",
+        },
+        {
+          title: "VOC2012 Object Detection Task",
+          subTitle: "http://github.com/l1xiangyi/dda4220-final/",
+          date: "May 2023",
+          description: "Analyzed images with classic OpenCV algorithms. Fine-tuned STOA models like Faster RCNN, SSD, and YOLO and compared performances. Wrote Bash scripts to train and log results for GPU clusters. "
+        },
+        {
+          title: "ChatGPT Detector",
+          subTitle: "https://github.com/l1xiangyi/csc3160-final/",
+          date: "May 2023",
+          description: "Fine-tuned RoBERTa models with scraped human texts and generated texts using a suite of different LLMs. Compared the results with the baseline model. Proved increased generalization performance by introducting more LLM texts. ",
         },
         {
           title: "Kanan Minimal",
