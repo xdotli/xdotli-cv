8cef7b223d223fccafc825d8637f6c2ac1cca8b8
Xiangyi Li <venusli370306@gmail.com>
2024-02-13 13:59:59 -0800

    Update general.ts

--- data/general.ts
+++ data/general.ts
@@ -4,7 +4,7 @@ export const generalData = {
   jobTitle: "Software Engineer, Student",
   website: "https://www.xiangyi.li",
   about:
-    "Hi, it's Xiangyi Li! I'm curretnly a Master student at SJSU. Head of engineering at Imaginix Inc. Building Kimi now.  ",
+    "Hi, it's Xiangyi Li! I'm curretnly a Master student at SJSU. Head of engineering at Imaginix Inc. Maker of Kimi AI Voice Chat. Available on App Store: https://apps.apple.com/us/app/kimi-ai-anime-voice-chat/id6477160641 ",
   contacts: [
     {
       label: "Email",
