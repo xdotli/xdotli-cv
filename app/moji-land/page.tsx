1faaa8d154485ed81b65933dc0fd936af14b8bb0
Xiangyi Li <venusli370306@gmail.com>
2024-04-08 02:21:46 -0700

    add moji land

---
+++ app/moji-land/page.tsx
@@ -0,0 +1,10 @@
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/NxIxHEjtlU1
+ * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
+ */
+import { MojiLand } from "@/components/moji-land";
+
+export default function MojiLandPage() {
+    return <MojiLand />
+}
