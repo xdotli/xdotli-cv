980f96664b37693fc5860eb1ef6d3df4ef6531a2
Xiangyi Li <venusli370306@gmail.com>
2024-06-05 16:45:39 -0700

    Embed Notion Cover Letters

---
+++ app/notion-cover-letters/software-engineer-product-infrastructure/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <iframe
+      src="https://neyexmoxg9qwy9.embednotionpage.com/Notion-CL-Software-Engineer-Product-Infrastructure-5361cd0c684c4c91a311b7c84c6a39c5"
+      style={{ width: "100%", height: "100vh", border: "none", padding: "0" }}
+    ></iframe>
+  );
+}
