3228f448aadab9cb3c9c158a15424f8d5f9159e7
xdotli <venusli370306@gmail.com>
2024-06-05 18:04:39 -0700

    Notion monetization role

---
+++ app/notion-cover-letters/software-engineer-monetization-experience/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+        <iframe
+            src="https://zqeq96v99yx9o3.embednotionpage.com/Xiangyi-Li-Software-Engineer-Monetization-Experience-cdc4e243196e494db9c34f07ec98a5b6"
+            style={{ width: "100%", height: "100vh", border: "none", padding: "0" }}
+        ></iframe>
+    );
+  }
+  
