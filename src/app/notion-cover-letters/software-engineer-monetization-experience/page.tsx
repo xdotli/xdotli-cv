d19a9c3f502a4f42e6837685809f3e7638f598f0
Xiangyi Li <venusli370306@gmail.com>
2024-06-17 11:06:09 -0700

    re point notion cover letters

---
+++ src/app/notion-cover-letters/software-engineer-monetization-experience/page.tsx
@@ -0,0 +1,7 @@
+// app/page.tsx
+import { redirect } from 'next/navigation';
+
+export default function Page() {
+  // Perform the redirect
+  redirect('https://xdotli.notion.site/Xiangyi-Li-Software-Engineer-Monetization-Experience-cdc4e243196e494db9c34f07ec98a5b6');
+}
---
+++ src/app/notion-cover-letters/software-engineer-product-infrastructure/page.tsx
@@ -0,0 +1,7 @@
+// app/page.tsx
+import { redirect } from 'next/navigation';
+
+export default function Page() {
+  // Perform the redirect
+  redirect('https://xdotli.notion.site/Xiangyi-Li-Software-Engineer-Product-Infrastructure-5361cd0c684c4c91a311b7c84c6a39c5');
+}
